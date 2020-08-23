180ee1a6e12e542da70abca1472bb10992260dbd
linxuhao <linxuhao84@gmail.com>
2020-08-23 02:12:51 +0200

    Updates

---
+++ precache-manifest.e5f622e76ac315cb967348acb9d768c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ab7de825b5a8a5b7bb95ab2c908c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de64bbb080448e9d21bf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d37f3f69d8f83eadc30d",
+    "url": "/static/js/2.246b6df7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.246b6df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de64bbb080448e9d21bf",
+    "url": "/static/js/main.989cfd9d.chunk.js"
+  },
+  {
+    "revision": "94605443efb9801c26ff",
+    "url": "/static/js/runtime-main.5c43b987.js"
+  }
+]);
